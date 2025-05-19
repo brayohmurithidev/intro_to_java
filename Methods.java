400846ca870cdc8ba808d88c5795f5671aa92f66
brayohmurithidev <murithibrianm@gmail.com>
2025-05-19 22:27:18 +0300

    method returns

--- Methods.java
+++ Methods.java
@@ -4,10 +4,16 @@ public class Methods {
         /*
         Static - needed to be able to use the method in main.
         void - means the method doesn't return a value
+        Methods can have parameters - they are methods variables.
+        Return values - results from methods
          */
-    //Methods can have parameters - they are methods variables.
+
     static void welcome(String name, int age) {
         System.out.println("Hello " + name + " and you are " + age);
+    }
+
+    static double perc(double num, int percentage) { // double is the returned value
+        return num * percentage / 100;
     }
 
 
@@ -15,5 +21,9 @@ public class Methods {
 // Call our method in the main class
         welcome("Brian", 27); //values passed on params are arguments
         welcome("Lewis", 18);
+        double result = perc(4, 25);
+        double res = perc(10, 25);
+        System.out.println(result);
+        System.out.println(res);
     }
 }
